31129646eb2c79d08092a8233d0594c16395d697
Wendymackennah <wendymackennah5@gmail.com>
2026-02-05 05:59:18 +0300

    switch names

---
+++ src/Data/circulationData.js
@@ -0,0 +1,5 @@
+export const circulationData = [
+  { name: "Issued Books", value: 60, color: "#06b6d4" },
+  { name: "Reserved Books", value: 25, color: "#facc15" },
+  { name: "Overdue Books", value: 15, color: "#ef4444" },
+];
