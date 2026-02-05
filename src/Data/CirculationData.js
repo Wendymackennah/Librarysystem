f448cd2ae9d5dfbbd43615ccbfa833a5a9b7d696
Wendymackennah <wendymackennah5@gmail.com>
2026-02-05 05:55:19 +0300

    Data-CirculationData

---
+++ src/Data/CirculationData.js
@@ -0,0 +1,7 @@
+export const tableData = [
+  { book: "Karim Jannat", author: "Don Alvard", pub: "HQ Publication", progress: 90 },
+  { book: "Abdul Samad", author: "Mac John", pub: "Pajeri", progress: 90 },
+  { book: "Will Sameed", author: "Asif Mahmud", pub: "NT Publication", progress: 90 },
+  { book: "Azaduzzaman Mia", author: "Den Carlegy", pub: "Cheq Publication", progress: 90 },
+  { book: "Hosneyara Gulbahar", author: "Mahmud Shohag", pub: "NT Publication", progress: 90 },
+];
