4b583286be15b06b898823867d4e41b3067a4c44
Wendymackennah <wendymackennah5@gmail.com>
2026-02-05 05:47:24 +0300

    Pages-dashboard

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,1 +1,24 @@
 
+// /src/pages/Dashboard.jsx
+import React from "react";
+import { Grid, Box, Toolbar } from "@mui/material";
+import StatCards from "../components/StatCards";
+import CirculationChart from "../components/CirculationChart";
+import BooksTable from "../components/BooksTable";
+
+export default function Dashboard() {
+  return (
+    <Box>
+      <Toolbar />
+      <StatCards />
+      <Grid container spacing={2} mt={1}>
+        <Grid item xs={12} md={5}>
+          <CirculationChart />
+        </Grid>
+        <Grid item xs={12} md={7}>
+          <BooksTable />
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
